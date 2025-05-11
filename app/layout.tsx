748341a308f97f6eefe12fa05b24ad5e829509c6
Abishkar <abishkarchapagain1@gmail.com>
2025-05-11 16:43:56 +0545

    Second commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'AbroadDesk Billing System',
+  description: 'Created with Abishkar',
+  generator: 'Abishkar',
 }
 
 export default function RootLayout({
